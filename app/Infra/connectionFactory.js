04735e80dabf5130f8d9aa1482f0d15cf9d436a0
Renato Sampaio <renatosampaio81@yahoo.com.br>
2021-02-18 09:55:15 -0300

    novo ajuste - Tentando conectar no banco Heroku usando expressao regular

--- app/Infra/connectionFactory.js
+++ app/Infra/connectionFactory.js
@@ -37,17 +37,52 @@ function createDBConnection() { //essa é a função embrublhada, que'não será
 
 	if(process.env.NODE_ENV == 'production') { //se nao tiver definido nenhuma variavel de ambiente NODE_ENV, estamos fazendo desenvolvimento (fim das contas é igual que o de cima)
 		console.log("agora sim conectei o BD remoto");
-		//Vou conectar ao Banco em Núvem que o Hiroku me deu, se eu substituir os dados abaixos eles ficarão publicos, entao vou criar uma var que vai remeter a variável de ambiente que o hiroku criou no meu servidor
+		
+		
 		var urlDeConexao = process.env.CLEARDB_DATABASE_URL;
+
+	        console.log(urlDeConexao);
+
+	        var grupos = urlDeConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
+        
+	        var user = grupos[1],
+        	    password = grupos[2],
+            	    host = grupos[3],
+            	    database = grupos[4];
+
+        	console.log(`User ${user}, password: ${password}, host: ${host}, database: ${database}`);
+             
+        	return  mysql.createConnection({            
+            	    host : host,
+            	    user : user,
+            	    password : password,
+        	    database : database
+	        });
+		
+
+
+		/*
+		//Vou conectar ao Banco em Núvem que o Hiroku me deu, se eu substituir os dados abaixos eles ficarão publicos, entao vou criar uma var que vai remeter a variável de ambiente que o hiroku criou no meu servidor
+		var url = process.env.CLEARDB_DATABASE_URL;
 		//aqui vou criar uma expressao regular para ir cortando cada campo dessa urlDeConexao e transformando em um array
-		var grupos = urlDeConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/); // regex semore entre "//"  depois comeca com sql: dai tem que skipar as barras, depois das barras vem o login, que é um grupo que vai ate o :  dai depois outro grupo que vai até o @
+		var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/); // regex semore entre "//"  depois comeca com sql: dai tem que skipar as barras, depois das barras vem o login, que é um grupo que vai ate o :  dai depois outro grupo que vai até o @
 		// o primeiro araay [0] é a expressao regular inteira SEMPRE
+				
 		return mysql.createConnection({ //a var connection vai estabelecer a conexão com o banco de dados
 			host: grupos[3], //em JSon, fazemos a configuração da chamada
 			user: grupos[1],
 			password: grupos[2],
 			database: grupos[4]	
 		});
+		
+		
+		return mysql.createConnection({ //a var connection vai estabelecer a conexão com o banco de dados
+			host: 'us-cdbr-east-03.cleardb.com', //em JSon, fazemos a configuração da chamada
+			user: 'b407b90d80320e',
+			password: 'bd71afb5',
+			database: 'heroku_0f6ecef09db5a41'	
+		});
+		*/
 	}
 }
 
