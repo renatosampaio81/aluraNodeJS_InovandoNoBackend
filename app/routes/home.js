dfa4c7d0ebc3f69a929b111552d17b347f7429ce
Renato Sampaio <renatosampaio81@yahoo.com.br>
2021-02-18 21:37:11 -0300

    ajustes

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,11 +2,11 @@
 
 module.exports = function(app) {
     app.get("/", function(request,response){ //assossia a raiz da aplicação "/" a uma função que irá tratar as requisições e as respostas
-        var connection = app.infra.connectionFactory()(); // estou chamando a conexão ao banco via caminho (funcionalidade do load express) em vez de via variável, pq agora o connectionFatory está no express-load
+        var connection = app.infra.connectionFactory(); // estou chamando a conexão ao banco via caminho (funcionalidade do load express) em vez de via variável, pq agora o connectionFatory está no express-load
         var produtosDAO = new app.infra.ProdutosDAO(connection); // carrega o modulo produtosBanco, passando connection como argumento
         //se não utilizar o new, o this, dentro da função construtora lá em produtosBranco.js, não funciona direito, não retorna esse objeto produtoBanco de uma forma correta
         produtosDAO.listagem(function(err,results) { //chama a função lista lá do produtosBranco, passando como argumento a function(err,results) é o callback
-            results = JSON.stringify(results);
+            //results = JSON.stringify(results); teste pra transformar em json
             response.render('home/index',{livros:results});
         });
         connection.end();    
