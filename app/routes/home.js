5492762bdb5156342b9ce9ba7fe3c9619f12b670
Renato Sampaio <renatosampaio81@yahoo.com.br>
2021-02-18 21:16:11 -0300

    ajuste no home

--- app/routes/home.js
+++ app/routes/home.js
@@ -6,6 +6,7 @@ module.exports = function(app) {
         var produtosDAO = new app.infra.ProdutosDAO(connection); // carrega o modulo produtosBanco, passando connection como argumento
         //se não utilizar o new, o this, dentro da função construtora lá em produtosBranco.js, não funciona direito, não retorna esse objeto produtoBanco de uma forma correta
         produtosDAO.listagem(function(err,results) { //chama a função lista lá do produtosBranco, passando como argumento a function(err,results) é o callback
+            results = JSON.stringify(results);
             response.render('home/index',{livros:results});
         });
         connection.end();    
